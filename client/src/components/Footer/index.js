4da0acb43fb58f2e9f1c04d3d3ac99024a59cdfb
Delph <delph_teb@hotmail.com>
2021-04-03 12:29:28 -0400

    basic app with 2 components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="center-align flex-column row footer p-3 bg-dark text-white">
+      <i>
+        &#169; 2021 Copyright 
+        <strong> 
+          <a href="https://github.com/Delph-Sunny"> DT</a>
+        </strong>
+      </i>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="text-white display-4 py-2">Google Books</div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="pl-4 ml-4 collapse navbar-collapse"
+        id="navbarNavAltMarkup"
+      >
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link text-white h3" to="/">
+            Search
+          </Link>
+          <Link className="nav-item nav-link text-white h3" to="/saved">
+            Saved
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
