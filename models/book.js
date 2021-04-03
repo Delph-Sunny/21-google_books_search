4c910912608d34fcf320607837634527d5127dc3
Delph <delph_teb@hotmail.com>
2021-04-03 12:06:48 -0400

    add ratings

--- models/book.js
+++ models/book.js
@@ -3,10 +3,12 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
+  subtitle: { type: String },
   authors: { type: Array, required: true },
   description: { type: String, required: true },
-  image:{ type: String, required: true },
-  link: { type: String, required: true }
+  image: { type: String, required: true },
+  link: { type: String, required: true },
+  averageRating: {type: Number}
 });
 
 const Book = mongoose.model("Book", bookSchema);
