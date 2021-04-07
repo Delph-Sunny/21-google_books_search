52e72be0f039ffcddd96b042fb930e61b384064f
Delph <delph_teb@hotmail.com>
2021-04-06 21:52:17 -0400

    clean up server

--- server.js
+++ server.js
@@ -15,12 +15,6 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Send every request to the React app
-/* Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});*/
-
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/googlebooks", {
   useCreateIndex: true,
